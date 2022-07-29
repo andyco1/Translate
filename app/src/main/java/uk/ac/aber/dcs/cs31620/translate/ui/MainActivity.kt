90db75ebbd87ebd199c5a527d510a6bce507a546
andyco1 <arc29@aber.ac.uk>
2022-07-29 19:31:52 +0100

    changed package structure and enabled view binding

---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package uk.ac.aber.dcs.cs31620.translate.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uk.ac.aber.dcs.cs31620.translate.R
+import uk.ac.aber.dcs.cs31620.translate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+    }
+}
