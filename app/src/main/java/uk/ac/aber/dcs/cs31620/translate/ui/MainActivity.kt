30880985eae4daa66ec29f0fef6bf07fb178e93f
andyco1 <arc29@aber.ac.uk>
2022-07-30 07:09:55 +0100

    Implemented 3 fragments for each tab on the navigation bar

--- app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/MainActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/MainActivity.kt
@@ -2,19 +2,32 @@ package uk.ac.aber.dcs.cs31620.translate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import uk.ac.aber.dcs.cs31620.translate.R
 import uk.ac.aber.dcs.cs31620.translate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_main)
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
+
+        val navView = binding.bottomNavView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_vocabulary, R.id.navigation_practice))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
