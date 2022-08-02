13334179384ff0d5461739d4d594828617259368
andyco1 <arc29@aber.ac.uk>
2022-08-02 01:16:34 +0100

    Progress in implementing spinners on the fragment_home.xml

--- app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/home/HomeFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package uk.ac.aber.dcs.cs31620.translate.ui.home
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import uk.ac.aber.dcs.cs31620.translate.R
 import uk.ac.aber.dcs.cs31620.translate.databinding.FragmentHomeBinding
 
@@ -13,6 +17,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeFragmentBinding: FragmentHomeBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +25,11 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         homeFragmentBinding =
             FragmentHomeBinding.inflate(inflater, container, false)
+
+        setupSpinner(view, homeFragmentBinding.nativeLanguageSpinner, R.array.NativeLanguages)
+        setupSpinner(view, homeFragmentBinding.foreignLanguageSpinner, R.array.ForeignLanguages)
+
+        
 
         return homeFragmentBinding.root
     }
--- app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/vocabulary/VocabularyFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/vocabulary/VocabularyFragment.kt
@@ -1,5 +1,6 @@
 package uk.ac.aber.dcs.cs31620.translate.ui.vocabulary
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,16 +11,19 @@ import uk.ac.aber.dcs.cs31620.translate.databinding.FragmentHomeBinding
 import uk.ac.aber.dcs.cs31620.translate.databinding.FragmentVocabularyBinding
 
 class VocabularyFragment : Fragment() {
-
     private lateinit var vocabularyFragmentBinding: FragmentVocabularyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         vocabularyFragmentBinding =
             FragmentVocabularyBinding.inflate(inflater, container, false)
+
+
+        // Insert code here
 
         return vocabularyFragmentBinding.root
     }
