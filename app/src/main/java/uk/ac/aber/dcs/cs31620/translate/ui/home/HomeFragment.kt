be5d93b4780e39ecde860abf1ff1c8eee6323f31
andyco1 <arc29@aber.ac.uk>
2022-08-02 02:19:25 +0100

    Progress in implementing spinners on the fragment_home.xml

--- app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/home/HomeFragment.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/ui/home/HomeFragment.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.widget.*
 import uk.ac.aber.dcs.cs31620.translate.R
 import uk.ac.aber.dcs.cs31620.translate.databinding.FragmentHomeBinding
 
@@ -29,9 +27,37 @@ class HomeFragment : Fragment() {
         setupSpinner(view, homeFragmentBinding.nativeLanguageSpinner, R.array.NativeLanguages)
         setupSpinner(view, homeFragmentBinding.foreignLanguageSpinner, R.array.ForeignLanguages)
 
-        
+
 
         return homeFragmentBinding.root
     }
 
+    private fun setupSpinner(view: View?, spinner: Spinner, arrayResourceId: Int) {
+        spinner.setSelection(1)
+
+        val adapter =
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                arrayResourceId,
+                android.R.layout.simple_spinner_item
+            )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // We don't need this but we have to provide
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(context, "Item $id selected", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
