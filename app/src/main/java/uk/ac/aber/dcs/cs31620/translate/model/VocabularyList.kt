888c86908c53a83e09cfcf55116e1db30b1c3af8
andyco1 <arc29@aber.ac.uk>
2022-08-06 03:37:44 +0100

    Added floating action point button

--- app/src/main/java/uk/ac/aber/dcs/cs31620/translate/model/VocabularyList.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/translate/model/VocabularyList.kt
@@ -4,6 +4,15 @@ class VocabularyList {
     val vocabList = listOf(
         Vocabulary("Hello", "Bonjour"),
         Vocabulary("Goodbye", "Au Revoir"),
-        Vocabulary("What's your name?", "Comment tu t'appelles?")
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
+        Vocabulary("What's your name?", "Comment tu t'appelles?"),
     )
 }
